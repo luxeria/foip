dca6a6afef7a798ce1d7bfb5ce76094abcc72e05
Nino Ninux <nino.ninux@gmail.com>
2015-07-25 20:26:24 +0200

    suppred useless output from wget

--- data/get_data.sh
+++ data/get_data.sh
@@ -3,6 +3,6 @@
 IMAGE_01_SOURCE="https://upload.wikimedia.org/wikipedia/commons/6/62/Ehemalige-Kantonsbibliothek-Luzern.jpg"
 IMAGE_01_NAME="building.jpg"
 
-echo "downloading image data from"
-
-wget $IMAGE_01_SOURCE -O $IMAGE_01_NAME
+# image 01
+echo "downloading image data from:" $IMAGE_01_SOURCE 
+wget $IMAGE_01_SOURCE -O $IMAGE_01_NAME -o /dev/null
